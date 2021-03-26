d3eede8a2bd97ff9aebd5ef252b77ed42702d35d
Scott Alton <scottaltonmusic@gmail.com>
2021-03-26 13:59:26 -0400

    Added project files from previous project.

--- app.ts
+++ app.ts
@@ -4,20 +4,21 @@ import path = require("path");
 import cookieParser = require("cookie-parser");
 import logger = require("morgan");
 
-let indexRouter = require("./routes/index");
+let indexRouter = require("./Routes/index");
 
 // Web app
 let app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "Views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Client")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
 app.use("/", indexRouter);
 
