5671a2b41aa40f5889577e8be15a8ba761010736
Scott Alton <scottaltonmusic@gmail.com>
2021-03-26 18:34:13 -0400

    Attempted redeploy after changing .listen in server.js.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require("http");
 let port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 const server = http.createServer(app);
-server.listen(port);
+app.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
 function normalizePort(val) {
