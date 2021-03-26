faf6208ae04bd69434cfbddbbfcc601afeae3046
Scott Alton <scottaltonmusic@gmail.com>
2021-03-26 14:20:13 -0400

    Converted all HTML files to EJS.

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,14 @@
 import express = require('express');
 let router = express.Router();
 
-/* GET home page. */
+/* GET home page - default route */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page - default route */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
